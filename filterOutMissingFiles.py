0abc35bc0f2d26a5680cfbd9190b2507ae288b85
intheflesh <romanf1@mail.afeka.ac.il>
2020-06-12 19:23:42 +0300

    added our re-created pre-train model with additional data preperation code

--- filterOutMissingFiles.py
+++ filterOutMissingFiles.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
-inFileCSV = open('Data_Entry_2017.csv',"r") #loading the csv file
-outFileCSV = open('Data_Entry_2017_Updated.csv',"w") #loading the csv file
-pathOfFiles = r'D:\Data\covid-caps-backup\covid-caps_16.05.20\database_preprocessed'
+
+# since there are some misssing files in the orignal 'Data_Entry_2017.csv" file, we create a new one based on the folder with the actual images
+inFileCSV = open('Data_Entry_2017.csv',"r")             # loading the original csv file
+outFileCSV = open('Data_Entry_2017_Updated.csv',"w")    # creating the final csv file
+pathOfFiles = r'D:\Data\covid-caps-backup\covid-caps_16.05.20\database_preprocessed'    # the folder where we keep the images
 files = os.listdir(pathOfFiles)
 first = inFileCSV.readline()
 outFileCSV.write(first)
